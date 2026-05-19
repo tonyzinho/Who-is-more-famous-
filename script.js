8e8834d5bbb5f6548ec5bc8a41621e0906ce944b
Antonio Paulo <105022020+tonyzinho@users.noreply.github.com>
2026-05-19 01:16:11 -0300

    Update script.js

--- script.js
+++ script.js
@@ -30,39 +30,104 @@ const artistas = [
 
 async function buscarArtista(nome){
 
-    const resposta = await fetch(
-        `https://corsproxy.io/?https://api.deezer.com/search/artist?q=${nome}`
-    )
+    try{
 
-    const dados = await resposta.json()
+        const resposta = await fetch(
 
-    const artista = dados.data[0]
+            `https://deezerdevs-deezer.p.rapidapi.com/search?q=${nome}`,
 
-    return {
+            {
 
-        nome: artista.name,
+                method:"GET",
 
-        imagem: artista.picture_big,
+                headers:{
 
-        seguidores: artista.nb_fan
+                    "x-rapidapi-host":
+                    "deezerdevs-deezer.p.rapidapi.com",
 
+                    "x-rapidapi-key":
+                    "e26b1ad218mshfcbcdd8e4bc5b9bp12827bjsne0c473b37007"
+
+                }
+
+            }
+
+        )
+
+        const dados = await resposta.json()
+
+        console.log(dados)
+
+        const artista = dados.data[0]
+
+        return {
+
+            nome:
+                artista.artist.name,
+
+            imagem:
+                artista.artist.picture_big,
+
+            seguidores:
+                Math.floor(
+                    Math.random() * 100000000
+                )
+
+        }
+
+    }
+
+    catch(error){
+
+        console.log(error)
+
+        alert(
+            "Erro ao buscar artista."
+        )
     }
 }
 
 async function novaRodada(){
 
     const nome1 =
+
         artistas[
-            Math.floor(Math.random() * artistas.length)
+            Math.floor(
+                Math.random() * artistas.length
+            )
         ]
 
-    const nome2 =
+    let nome2 =
+
         artistas[
-            Math.floor(Math.random() * artistas.length)
+            Math.floor(
+                Math.random() * artistas.length
+            )
         ]
+
+    while(nome1 === nome2){
+
+        nome2 =
+
+            artistas[
+                Math.floor(
+                    Math.random() * artistas.length
+                )
+            ]
+    }
 
     artista1 = await buscarArtista(nome1)
+
     artista2 = await buscarArtista(nome2)
+
+    if(!artista1 || !artista2){
+
+        alert(
+            "Erro ao carregar artistas."
+        )
+
+        return
+    }
 
     document.getElementById("img1")
         .src = artista1.imagem
@@ -79,21 +144,34 @@ async function novaRodada(){
 
 function escolher(opcao){
 
+    if(!artista1 || !artista2){
+
+        return
+    }
+
     let acertou = false
 
     if(
+
         opcao === 1 &&
+
         artista1.seguidores >
         artista2.seguidores
+
     ){
+
         acertou = true
     }
 
     if(
+
         opcao === 2 &&
+
         artista2.seguidores >
         artista1.seguidores
+
     ){
+
         acertou = true
     }
 
@@ -102,43 +180,66 @@ function escolher(opcao){
         score++
 
         alert(
+
             "Acertou!\n\n" +
 
             artista1.nome +
+
             ": " +
+
             artista1.seguidores.toLocaleString()
+
             +
+
             " fãs\n\n" +
 
             artista2.nome +
+
             ": " +
+
             artista2.seguidores.toLocaleString()
+
             +
+
             " fãs"
+
         )
 
-    }else{
+    }
 
-        score = 0
+    else{
 
         alert(
+
             "Errou!\n\n" +
 
             artista1.nome +
+
             ": " +
+
             artista1.seguidores.toLocaleString()
+
             +
+
             " fãs\n\n" +
 
             artista2.nome +
+
             ": " +
+
             artista2.seguidores.toLocaleString()
+
             +
+
             " fãs"
+
         )
+
+        score = 0
     }
 
     document.getElementById("score")
+
         .innerText = `Score: ${score}`
 
     novaRodada()
